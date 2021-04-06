aa2727d387018bef25d2e5de122ff39037bfcdd6
Bruno Brasil <bruno.brasilf@gmail.com>
2021-04-06 20:27:55 -0300

    chore: setup material-ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import Button from '@material-ui/core/Button'
+
 export default function Home() {
   return (
     <div>
       <h1>AVA-LON</h1>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
     </div>
   )
 }
