92969eaf43224fd6d90cdb594e3156d6aefe8e1c
Bruno Brasil <bruno.brasilf@gmail.com>
2021-04-12 19:47:12 -0300

    feat: create SubjectCard component

---
+++ src/components/Subject/SubjectCard.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import {
+  makeStyles,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography
+} from '@material-ui/core'
+
+import Subject from '../../models/subject'
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345
+  },
+  media: {
+    height: 140
+  }
+})
+
+interface Props {
+  subject: Subject
+}
+
+export default function SubjectCard({ subject }: Props) {
+  const classes = useStyles()
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={subject.image}
+          title={subject.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {subject.title}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  )
+}
---
+++ src/models/subject.ts
@@ -0,0 +1,5 @@
+export default interface Subject {
+  id: string
+  title: string
+  image: string
+}
