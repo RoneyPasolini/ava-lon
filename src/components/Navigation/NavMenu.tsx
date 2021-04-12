635921ea3d6f87ecdf20c3a74dbe618d15a8c4d7
Bruno Brasil <bruno.brasilf@gmail.com>
2021-04-11 23:27:38 -0300

    feat: create styles for NavMenu

--- src/components/Navigation/NavMenu.tsx
+++ src/components/Navigation/NavMenu.tsx
@@ -1,15 +1,38 @@
 import {
+  createStyles,
   Divider,
   Drawer,
   List,
   ListItem,
-  ListItemText
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  Theme
 } from '@material-ui/core'
 
 import Link from 'next/link'
 import { useContext } from 'react'
 import { NavMenuContext } from '../../contexts/NavMenuContext'
 import CloseMenuButton from './CloseMenuButton'
+
+const drawerWidth = 240
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    drawer: {
+      width: drawerWidth
+    },
+    drawerPaper: {
+      width: drawerWidth,
+      textAlign: 'center'
+    },
+    drawerHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0, 1)
+    }
+  })
+)
 
 export interface NavItem {
   text: string
@@ -23,16 +46,27 @@ interface Props {
 
 export function NavMenu({ navItems }: Props) {
   const { isOpen } = useContext(NavMenuContext)
+  const classes = useStyles()
 
   return (
-    <Drawer variant="persistent" anchor="left" open={isOpen}>
-      <CloseMenuButton />
+    <Drawer
+      variant="persistent"
+      anchor="left"
+      open={isOpen}
+      className={classes.drawer}
+      classes={{
+        paper: classes.drawerPaper
+      }}
+    >
+      <div className={classes.drawerHeader}>
+        <CloseMenuButton />
+      </div>
       <Divider />
       <List>
         {navItems.map((item) => (
           <Link href={item.url} key={item.text}>
             <ListItem button>
-              {item.icon}
+              <ListItemIcon>{item.icon}</ListItemIcon>
               <ListItemText primary={item.text} />
             </ListItem>
           </Link>
