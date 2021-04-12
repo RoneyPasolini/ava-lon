4f8c3bf90a47105b5d06e5210bd282ce93c9294b
Bruno Brasil <bruno.brasilf@gmail.com>
2021-04-12 19:48:56 -0300

    feat: add SubjectGrid to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,25 @@
-import Button from '@material-ui/core/Button'
+import SubjectGrid from '../components/Subject/SubjectGrid'
 import PageLayout from '../components/Layout/PageLayout'
+import Subject from '../models/subject'
+
+const subjects = [
+  {
+    id: '001',
+    title: 'Matemática',
+    image: '/img/math.jpg'
+  },
+  {
+    id: '002',
+    title: 'Ciência',
+    image: '/img/science.jpg'
+  }
+] as Subject[]
 
 export default function Home() {
   return (
     <PageLayout>
-      <h1>AVA-LON</h1>
-      <Button variant="contained" color="primary">
-        Click Me
-      </Button>
+      <h1>Matérias</h1>
+      <SubjectGrid subjects={subjects} />
     </PageLayout>
   )
 }
