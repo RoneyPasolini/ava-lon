b31642cc8b12bcfebab7644835ac62d111b532a2
Bruno Brasil <bruno.brasilf@gmail.com>
2021-04-13 22:22:36 -0300

    feat: save preferred theme on local storage

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,5 +1,5 @@
 import { Theme } from '@material-ui/core'
-import { createContext, ReactNode, useState } from 'react'
+import { createContext, ReactNode, useEffect, useState } from 'react'
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import { themes } from '../themes'
 
@@ -17,11 +17,24 @@ export const ThemeContext = createContext({} as ThemeContextData)
 export const ThemeProvider = ({ children }: Props) => {
   const [theme, setTheme] = useState(themes.current)
 
+  const restoreTheme = () => {
+    const storedThemeType = localStorage.getItem('theme') as string
+    if (storedThemeType in themes && theme.palette.type !== storedThemeType) {
+      themes.current = themes[storedThemeType]
+      setTheme(themes.current)
+    }
+  }
+
   const changeTheme = () => {
     themes.current =
       themes.current.palette.type === 'dark' ? themes.light : themes.dark
     setTheme(themes.current)
+    localStorage.setItem('theme', themes.current.palette.type)
   }
+
+  useEffect(() => {
+    restoreTheme()
+  }, [])
 
   return (
     <ThemeContext.Provider value={{ theme, changeTheme }}>
