8695b5ee2a4d03b24dcc9d91e8c3c90e95c2ab1c
bruno-br <bruno.brasilf@gmail.com>
2021-08-16 19:53:00 -0300

    feat: add route to create user

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,14 @@
 import { Request, Response, Router } from "express";
+import { CreateUserController } from "./controllers/CreateUserController";
 
 const routes = Router();
+
+const createUserController = new CreateUserController();
 
 routes.get("/", (req: Request, res: Response) => {
   return res.status(200).json({ message: "Hello World!" });
 });
+
+routes.post("/users", createUserController.handle);
 
 export { routes };
