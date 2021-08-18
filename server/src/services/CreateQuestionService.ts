660aeb77de411081da1433dd31d6f3d222f3e88c
bruno-br <bruno.brasilf@gmail.com>
2021-08-18 20:53:24 -0300

    refactor: id and answers are not defined on createQuestionService

--- server/src/services/CreateQuestionService.ts
+++ server/src/services/CreateQuestionService.ts
@@ -1,15 +1,12 @@
 import { getRepository } from "typeorm";
 import { Question } from "../entities/Question";
-import { v4 as uuidv4 } from "uuid";
 
 class CreateQuestionService {
   async execute(text: string) {
     const questionsRepository = await getRepository(Question);
 
     const question = await questionsRepository.create({
-      id: uuidv4(),
       text,
-      answers: [],
     });
 
     await questionsRepository.save(question);
