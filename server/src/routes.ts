7ca5bab3d134e53a590239538df85d9c96ea8841
bruno-br <bruno.brasilf@gmail.com>
2021-08-18 20:47:06 -0300

    feat: add create question route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from "express";
+import { CreateQuestionController } from "./controllers/CreateQuestionController";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { ListUsersController } from "./controllers/ListUsersController";
 
@@ -7,7 +8,11 @@ const routes = Router();
 const createUserController = new CreateUserController();
 const listUsersController = new ListUsersController();
 
+const createQuestionController = new CreateQuestionController();
+
 routes.post("/users", createUserController.handle);
 routes.get("/users", listUsersController.handle);
+
+routes.post("/questions", createQuestionController.handle);
 
 export { routes };
