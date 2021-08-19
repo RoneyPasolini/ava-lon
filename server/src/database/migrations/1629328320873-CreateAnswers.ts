9cee81115ae15473faff7d21d9f80dac16e45886
bruno-br <bruno.brasilf@gmail.com>
2021-08-18 21:11:52 -0300

    feat: create answers table migration

---
+++ server/src/database/migrations/1629328320873-CreateAnswers.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateAnswers1629328320873 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "answers",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "question",
+            type: "uuid",
+          },
+          {
+            name: "text",
+            type: "varchar",
+          },
+          {
+            name: "isCorrect",
+            type: "boolean",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FKAnswerQuestion",
+            referencedTableName: "questions",
+            referencedColumnNames: ["id"],
+            columnNames: ["question"],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("answers");
+  }
+}
