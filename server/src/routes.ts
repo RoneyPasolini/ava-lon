e63fd343c3a987f2e128e55308c0a3cf90bf586c
bruno-br <bruno.brasilf@gmail.com>
2021-08-19 21:47:44 -0300

    feat: create route to get question by id

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,14 @@
 import { Request, Response, Router } from "express";
 import { CreateQuestionController } from "./controllers/CreateQuestionController";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { GetQuestionController } from "./controllers/GetQuestionController";
 import { ListUsersController } from "./controllers/ListUsersController";
 
 const routes = Router();
 
 const createUserController = new CreateUserController();
 const listUsersController = new ListUsersController();
+const getQuestionController = new GetQuestionController();
 
 const createQuestionController = new CreateQuestionController();
 
@@ -14,5 +16,6 @@ routes.post("/users", createUserController.handle);
 routes.get("/users", listUsersController.handle);
 
 routes.post("/questions", createQuestionController.handle);
+routes.get("/questions/:id", getQuestionController.handle);
 
 export { routes };
