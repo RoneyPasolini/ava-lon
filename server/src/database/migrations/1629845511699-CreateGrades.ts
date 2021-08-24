033158175cbb41d479630eb47edd26a7ec814537
bruno-br <bruno.brasilf@gmail.com>
2021-08-24 20:04:53 -0300

    feat: create grades table migration

---
+++ server/src/database/migrations/1629845511699-CreateGrades.ts
@@ -0,0 +1,57 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateGrades1629845511699 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "grades",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "max_value",
+            type: "float",
+          },
+          {
+            name: "value_earned",
+            type: "float",
+            isNullable: true,
+          },
+          {
+            name: "activity",
+            type: "uuid",
+          },
+          {
+            name: "student",
+            type: "uuid",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FKGradeActivity",
+            referencedTableName: "activities",
+            referencedColumnNames: ["id"],
+            columnNames: ["activity"],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+          {
+            name: "FKGradeStudent",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["student"],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("grades");
+  }
+}
