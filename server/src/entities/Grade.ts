a5843516a7391fae685514950c0aceda5ba358c3
bruno-br <bruno.brasilf@gmail.com>
2021-08-24 20:01:18 -0300

    feat: create grade entity

--- server/src/entities/Grade.ts
+++ server/src/entities/Grade.ts
@@ -1,7 +1,21 @@
-import { Entity, PrimaryColumn } from "typeorm";
+import { Column, Entity, OneToOne, PrimaryColumn } from "typeorm";
+import { Activity } from "./Activity";
+import { User } from "./User";
 
 @Entity("grades")
 export class Grade {
   @PrimaryColumn()
   readonly id?: string;
+
+  @Column()
+  max_value!: number;
+
+  @Column()
+  value_earned!: number;
+
+  @OneToOne(() => Activity)
+  actitivy!: Activity;
+
+  @OneToOne(() => User)
+  student!: User;
 }
