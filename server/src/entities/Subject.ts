bfe5896ebf70a61816d2da833cec465cc07e1f54
bruno-br <bruno.brasilf@gmail.com>
2021-08-25 19:40:41 -0300

    feat: create subject entity

--- server/src/entities/Subject.ts
+++ server/src/entities/Subject.ts
@@ -1,7 +1,28 @@
-import { Entity, PrimaryColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  PrimaryColumn,
+} from "typeorm";
+import { Module } from "./Module";
+import { User } from "./User";
 
 @Entity("subjects")
 export class Subject {
   @PrimaryColumn()
   readonly id?: string;
+
+  @Column()
+  title!: string;
+
+  @ManyToOne(() => User, (user) => user.subjectsTeached)
+  teacher!: User;
+
+  @ManyToMany(() => User, (user) => user.subjectsStudied)
+  students!: User[];
+
+  @OneToMany(() => Module, (module) => module.subject)
+  modules!: Module[];
 }
