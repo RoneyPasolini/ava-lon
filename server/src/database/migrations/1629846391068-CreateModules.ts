b416c8db5883a93e718a226204ebec286fbf2f40
bruno-br <bruno.brasilf@gmail.com>
2021-08-25 19:28:48 -0300

    feat: create modules table migration

---
+++ server/src/database/migrations/1629846391068-CreateModules.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateModules1629846391068 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "modules",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "title",
+            type: "varchar",
+          },
+          {
+            name: "subject",
+            type: "uuid",
+          },
+          {
+            name: "activities",
+            type: "uuid",
+            isNullable: true,
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FKModuleSubject",
+            referencedTableName: "subjects",
+            referencedColumnNames: ["id"],
+            columnNames: ["subject"],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+          {
+            name: "FKModuleActivity",
+            referencedTableName: "activities",
+            referencedColumnNames: ["id"],
+            columnNames: ["activities"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("modules");
+  }
+}
