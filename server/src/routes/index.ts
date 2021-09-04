a5a929a7364ec2d50bfb130d0b048f704c750721
bruno-br <bruno.brasilf@gmail.com>
2021-09-04 17:21:40 -0300

    feat: add modules/activities routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,11 +3,15 @@ import { Router } from "express";
 import { usersRoutes } from "./users.routes";
 import { questionsRoutes } from "./questions.routes";
 import { subjectsRoutes } from "./subjects.routes";
+import { activitiesRoutes } from "./activities.routes";
+import { modulesRoutes } from "./modules.routes";
 
 const routes = Router();
 
 routes.use("/users", usersRoutes);
 routes.use("/questions", questionsRoutes);
 routes.use("/subjects", subjectsRoutes);
+routes.use("/activities", activitiesRoutes);
+routes.use("/modules", modulesRoutes);
 
 export { routes };
