26425231cffa7672095767c6f2e5b86f1bf16f26
bruno-br <bruno.brasilf@gmail.com>
2021-09-07 11:39:52 -0300

    feat: create route to get quiz

--- server/src/routes/activities.routes.ts
+++ server/src/routes/activities.routes.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import { CreateActivityController } from "../controllers/CreateActivityController";
 import { CreateQuizController } from "../controllers/CreateQuizController";
+import { GetQuizController } from "../controllers/GetQuizController";
 
 const createQuizController = new CreateQuizController();
+const getQuizController = new GetQuizController();
 const createActivityController = new CreateActivityController();
 
 const activitiesRoutes = Router();
 
 activitiesRoutes.post("/", createActivityController.handle);
+
 activitiesRoutes.post("/quiz", createQuizController.handle);
+activitiesRoutes.get("/quiz/:quiz_id", getQuizController.handle);
 
 export { activitiesRoutes };
